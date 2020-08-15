1b6c2071686b4c69e1798a54645dcee9280b41db
Joon-S <niceweather94@gmail.com>
2020-08-15 23:17:49 +0900

    기초 환경설정 세팅

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+interface Page {
+	color: string;
+}
+
+const App = ({ color }: Page): JSX.Element => {
+	return (
+		<>
+			<h1>Welcome to React with Typescript</h1>
+			<p>The color of this page is: {color}</p>
+		</>
+	);
+};
+
+export default App;
