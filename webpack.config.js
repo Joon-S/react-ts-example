fe50c3a9898d52d850c12cf50e750721f8ff95e3
Joon-S <niceweather94@gmail.com>
2020-10-07 15:02:26 +0900

    테스트커밋#2

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 module.exports = {
-  entry: './src/index.tsx',
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js']
-  },
-  output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'bundle.min.js'
-  },
-  module: {
-    rules: [
-      { 
-        test: /\.tsx?$/, 
-        loader: 'awesome-typescript-loader'
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ]
-}
+	entry: './src/index.tsx',
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js'],
+	},
+	output: {
+		path: path.join(__dirname, '/dist'),
+		filename: 'bundle.min.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: 'awesome-typescript-loader',
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+		}),
+	],
+};
