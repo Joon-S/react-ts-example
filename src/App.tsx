034cf3d3c705686239a79434fc764b7082569204
Joon-S <niceweather94@gmail.com>
2020-10-07 15:03:51 +0900

    그냥 커밋

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import * as React from 'react';
 const App: React.FC = () => {
 	return (
 		<>
-			<div>dhehheeee</div>
-		</>
-	);
+			<div>dhehheeee</div></>
+	)
 };
 
-export default App;
+export default App
 
-console.log(1);
+console.log(1);;;;
+
+
+
+
